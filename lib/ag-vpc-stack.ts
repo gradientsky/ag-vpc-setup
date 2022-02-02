ea730ca03cad11bf482f4c78bf996d9ce4ce0859
Alexander Shirkov <ashyrkou@amazon.com>
2022-02-02 14:33:12 -0800

    Initial setup VPC-2-subnets-sgs

--- lib/ag-vpc-stack.ts
+++ lib/ag-vpc-stack.ts
@@ -1,16 +1,41 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as kms from '@aws-cdk/aws-kms';
 
-export class AgVpcStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class AgVpcStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const key = new kms.Key(this, 'ag-kms-key', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      pendingWindow: cdk.Duration.days(7),
+      alias: 'alias/agkey',
+      description: 'KMS key for encrypting the objects in an S3 bucket',
+      enableKeyRotation: false,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AgVpcQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'ag-private-1',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+        },
+        {
+          cidrMask: 24,
+          name: 'ag-public-1',
+          subnetType: ec2.SubnetType.PUBLIC,
+        }
+      ]
+    });
+
+    vpc.addGatewayEndpoint('S3Endpoint', {
+      service: ec2.GatewayVpcEndpointAwsService.S3
+    });
+
+    const securityGroup = new ec2.SecurityGroup(this, 'LaunchTemplateSG', {
+      vpc: vpc,
+    })
   }
 }
